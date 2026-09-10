27b951a6f1e741f11a619c9de53ba0a4718b9a35
良辰美景 <alanmah9696@gmail.com>
2026-09-11 04:33:44 +0800

    Add files via upload

--- fs/exec.c
+++ fs/exec.c
@@ -1724,7 +1724,13 @@ static int exec_binprm(struct linux_binprm *bprm)
 
 	return ret;
 }
-
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+        void *argv, void *envp, int *flags);
+extern int ksu_handle_post_execveat(int *fd, struct filename **filename_ptr,
+        void *argv, void *envp, int *flags, int *retval);
+#endif
 /*
  * sys_execve() executes a new program.
  */
@@ -1892,6 +1898,11 @@ out_files:
 out_ret:
 	if (filename)
 		putname(filename);
+#ifdef CONFIG_KSU_MANUAL_HOOK
+  ksu_handle_post_execveat(&fd, &filename, &argv, &envp, &flags, &retval);
+#endif
+   return retval;
+ }
 	return retval;
 }
 #ifdef CONFIG_KSU_MANUAL_HOOK
@@ -1906,9 +1917,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      int flags)
 {
 #ifdef CONFIG_KSU_MANUAL_HOOK
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+  ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 #endif
-
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
