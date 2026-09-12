7164dc2e128852a82f236602387032686f9e8616
良辰美景 <alanmah9696@gmail.com>
2026-09-13 07:02:29 +0800

    Update focaltech_core.c

--- drivers/input/touchscreen/focaltech_touch/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_core.c
@@ -48,11 +48,11 @@
 #endif
 #include "focaltech_core.h"
 #undef FTS_INFO
-#define FTS_INFO(fmt, ...) do { } while (0)
+#define FTS_INFO(fmt, ...) do {} while (0)
 #undef FTS_DEBUG
-#define FTS_DEBUG(fmt, ...) do { } while (0)
+#define FTS_DEBUG(fmt, ...) do {} while (0)
 #undef FTS_ERROR
-#define FTS_ERROR(fmt, ...) do { } while (0)
+#define FTS_ERROR(fmt, ...) do {} while (0)
 /*****************************************************************************
 * Private constant and macro definitions using #define
 *****************************************************************************/
