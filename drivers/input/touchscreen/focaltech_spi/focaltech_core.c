f193bd87d666103db105f9a1aeee31324d811328
良辰美景 <alanmah9696@gmail.com>
2026-09-13 06:58:20 +0800

    Add files via upload

--- drivers/input/touchscreen/focaltech_spi/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_spi/focaltech_core.c
@@ -44,7 +44,12 @@
 #define FTS_SUSPEND_LEVEL 1 /* Early-suspend level */
 #endif
 #include "focaltech_core.h"
-
+#undef FTS_INFO
+#define FTS_INFO(fmt, ...) do {} while (0)
+#undef FTS_DEBUG
+#define FTS_DEBUG(fmt, ...) do {} while (0)
+#undef FTS_ERROR
+#define FTS_ERROR(fmt, ...) do {} while (0)
 /*****************************************************************************
 * Private constant and macro definitions using #define
 *****************************************************************************/
