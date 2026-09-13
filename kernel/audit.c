215cb554f32335519fe6addc740d5cbae36d869d
良辰美景 <alanmah9696@gmail.com>
2026-09-14 05:08:16 +0800

    Add files via upload

--- kernel/audit.c
+++ kernel/audit.c
@@ -1810,12 +1810,11 @@ static inline void audit_get_stamp(struct audit_context *ctx,
  */
 struct audit_buffer *audit_log_start(struct audit_context *ctx, gfp_t gfp_mask,
 				     int type)
-{
-    return NULL;
+{ 
 	struct audit_buffer *ab;
 	struct timespec64 t;
 	unsigned int serial;
-
+    return NULL;
 	if (audit_initialized != AUDIT_INITIALIZED)
 		return NULL;
 
