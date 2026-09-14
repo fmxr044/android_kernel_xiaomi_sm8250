4c72bce663ca14b4d20b61d51c44ba76264c41dd
良辰美景 <alanmah9696@gmail.com>
2026-09-14 09:23:45 +0800

    Add files via upload

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -30,6 +30,12 @@
 
 #include "base.h"
 #include "power/power.h"
+#undef dev_dbg
+#define dev_dbg(dev, fmt, ...) do {} while (0)
+#undef dev_info
+#define dev_info(dev, fmt, ...) do {} while (0)
+#undef dev_warn
+#define dev_warn(dev, fmt, ...) do {} while (0)
 
 /*
  * Deferred Probe infrastructure.
