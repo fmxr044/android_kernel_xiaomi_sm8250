2784f5eefff97ee4a3e6ab014c4b2c4748db3c52
良辰美景 <alanmah9696@gmail.com>
2026-09-15 10:55:58 +0800

    Update core.c

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -30,13 +30,13 @@
 #include "base.h"
 #include "power/power.h"
 #undef dev_info
-#define dev_info(dev, fmt, ...) do {} while (0)
+#define dev_info(...) do {} while (0)
 #undef dev_dbg
-#define dev_dbg(dev, fmt, ...) do {} while (0)
+#define dev_dbg(...) do {} while (0)
 #undef dev_err
-#define dev_err(dev, fmt, ...) do {} while (0)
+#define dev_err(...) do {} while (0)
 #undef dev_warn
-#define dev_warn(dev, fmt, ...) do {} while (0)
+#define dev_warn(...) do {} while (0)
 #ifdef CONFIG_SYSFS_DEPRECATED
 #ifdef CONFIG_SYSFS_DEPRECATED_V2
 long sysfs_deprecated = 1;
