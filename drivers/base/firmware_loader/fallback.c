cfadde4ad7253d9d6c25653c3364a9d9fb9d3cbf
良辰美景 <alanmah9696@gmail.com>
2026-09-15 10:57:18 +0800

    Update fallback.c

--- drivers/base/firmware_loader/fallback.c
+++ drivers/base/firmware_loader/fallback.c
@@ -13,11 +13,11 @@
 #include "fallback.h"
 #include "firmware.h"
 #undef dev_dbg
-#define dev_dbg(dev, fmt, ...) do {} while (0)
+#define dev_dbg(...) do {} while (0)
 #undef dev_err
-#define dev_err(dev, fmt, ...) do {} while (0)
+#define dev_err(...) do {} while (0)
 #undef dev_warn
-#define dev_warn(fmt, args...) do {} while (0)
+#define dev_warn(...) do {} while (0)
 
 /*
  * firmware fallback mechanism
