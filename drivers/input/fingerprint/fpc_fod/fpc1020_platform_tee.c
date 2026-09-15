2602538b564c11be1a5fdf83a1db053bf3b371b5
良辰美景 <alanmah9696@gmail.com>
2026-09-15 11:03:15 +0800

    Update fpc1020_platform_tee.c

--- drivers/input/fingerprint/fpc_fod/fpc1020_platform_tee.c
+++ drivers/input/fingerprint/fpc_fod/fpc1020_platform_tee.c
@@ -39,11 +39,11 @@
 #include <linux/fb.h>
 #include <linux/bitmap.h>
 #undef dev_info
-#define dev_info(dev, fmt, ...) do {} while (0)
+#define dev_info(...) do {} while (0)
 #undef dev_dbg
-#define dev_dbg(dev, fmt, ...) do {} while (0)
+#define dev_dbg(...) do {} while (0)
 #undef dev_err
-#define dev_err(dev, fmt, ...) do {} while (0)
+#define dev_err(...) do {} while (0)
 
 #define FPC_3V
 static struct regulator *p_2v9_vreg = NULL;
