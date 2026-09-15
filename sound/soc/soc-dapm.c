6383a7abd3dbd2a6088e6c9f50dfca1143faacc7
良辰美景 <alanmah9696@gmail.com>
2026-09-15 11:29:29 +0800

    Update soc-dapm.c

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -49,13 +49,13 @@
 	for ((dir) = SND_SOC_DAPM_DIR_IN; (dir) <= SND_SOC_DAPM_DIR_OUT; \
 		(dir)++)
 #undef dev_info
-#define dev_info(dev, fmt, ...) do {} while (0)
+#define dev_info(...) do {} while (0)
 #undef dev_err
-#define dev_err(dev, fmt, ...) do {} while (0)
+#define dev_err(...) do {} while (0)
 #undef dev_dbg
-#define dev_dbg(dev, fmt, ...) do {} while (0)
+#define dev_dbg(...) do {} while (0)
 #undef dev_warn
-#define dev_warn(dev, fmt, ...) do {} while (0)
+#define dev_warn(...) do {} while (0)
 static int snd_soc_dapm_add_path(struct snd_soc_dapm_context *dapm,
 	struct snd_soc_dapm_widget *wsource, struct snd_soc_dapm_widget *wsink,
 	const char *control,
