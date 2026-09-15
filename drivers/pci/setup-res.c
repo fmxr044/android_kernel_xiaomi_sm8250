a2f898e6030870dc7df02925d39272db8c875b1d
良辰美景 <alanmah9696@gmail.com>
2026-09-15 11:09:10 +0800

    Update setup-res.c

--- drivers/pci/setup-res.c
+++ drivers/pci/setup-res.c
@@ -22,9 +22,9 @@
 #include <linux/slab.h>
 #include "pci.h"
 #undef pci_info
-#define pci_info(dev, fmt, ...) do {} while (0)
+#define pci_info(...) do {} while (0)
 #undef pci_err
-#define pci_err(dev, fmt, ...) do {} while (0)
+#define pci_err(...) do {} while (0)
 static void pci_std_update_resource(struct pci_dev *dev, int resno)
 {
 	struct pci_bus_region region;
