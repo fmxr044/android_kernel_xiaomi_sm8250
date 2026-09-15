1b683a2ecba5c897e3ad0f63f15d89b8158689a9
良辰美景 <alanmah9696@gmail.com>
2026-09-15 11:02:35 +0800

    Update fpc1020_tee.c

--- drivers/input/fingerprint/fpc/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc/fpc1020_tee.c
@@ -71,11 +71,11 @@
 
 #define HWMON_CONPONENT_NAME "fingerprint"
 #undef dev_info
-#define dev_info(dev, fmt, ...) do {} while (0)
+#define dev_info(...) do {} while (0)
 #undef dev_dbg
-#define dev_dbg(dev, fmt, ...) do {} while (0)
+#define dev_dbg(...) do {} while (0)
 #undef dev_err
-#define dev_err(dev, fmt, ...) do {} while (0)
+#define dev_err(...) do {} while (0)
 static const char *const pctl_names[] = {
 	"fpc1020_reset_reset",
 	"fpc1020_reset_active",
