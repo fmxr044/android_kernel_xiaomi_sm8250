83e38f944e19abf567e36c1598d3e641bcdcf441
良辰美景 <alanmah9696@gmail.com>
2026-09-15 11:20:20 +0800

    Update ci13xxx_udc.c

--- drivers/usb/gadget/ci13xxx_udc.c
+++ drivers/usb/gadget/ci13xxx_udc.c
@@ -80,11 +80,11 @@
 #define MAX_PRIME_CHECK_RETRY	3 /*Wait for 3sec for EP prime failure */
 #define EXTRA_ALLOCATION_SIZE	256
 #undef dev_info
-#define dev_info(dev, fmt, ...) do {} while (0)
+#define dev_info(...) do {} while (0)
 #undef dev_err
-#define dev_err(dev, fmt, ...) do {} while (0)
+#define dev_err(...) do {} while (0)
 #undef dev_dbg
-#define dev_dbg(dev, fmt, ...) do {} while (0)
+#define dev_dbg(...) do {} while (0)
 /* ctrl register bank access */
 static DEFINE_SPINLOCK(udc_lock);
 
