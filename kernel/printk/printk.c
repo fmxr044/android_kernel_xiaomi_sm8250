a22713dff34a09b58127b0a422fbc780d7dcdab0
良辰美景 <alanmah9696@gmail.com>
2026-09-19 14:00:37 +0800

    Add files via upload

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -15,7 +15,7 @@
  * Rewrote bits to get rid of console_lock
  *	01Mar01 Andrew Morton
  */
-#define BUILD_PRINTK_C
+
 #include <linux/kernel.h>
 #include <linux/mm.h>
 #include <linux/tty.h>
@@ -778,36 +778,37 @@ struct devkmsg_user {
 
 static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 {
-	char *buf, *line;
-	int level = default_message_loglevel;
-	int facility = 1;	/* LOG_USER */
-	struct file *file = iocb->ki_filp;
-	struct devkmsg_user *user = file->private_data;
-	size_t len = iov_iter_count(from);
-	ssize_t ret = len;
+    return iov_iter_count(from);
+	//char *buf, *line;
+	//int level = default_message_loglevel;
+	//int facility = 1;	/* LOG_USER */
+	//struct file *file = iocb->ki_filp;
+	//struct devkmsg_user *user = file->private_data;
+	//size_t len = iov_iter_count(from);
+	//ssize_t ret = len;
 
-	if (!user || len > LOG_LINE_MAX)
-		return -EINVAL;
+	//if (!user || len > LOG_LINE_MAX)
+		//return -EINVAL;
 
 	/* Ignore when user logging is disabled. */
-	if (devkmsg_log & DEVKMSG_LOG_MASK_OFF)
-		return len;
+	//if (devkmsg_log & DEVKMSG_LOG_MASK_OFF)
+		//return len;
 
 	/* Ratelimit when not explicitly enabled. */
-	if (!(devkmsg_log & DEVKMSG_LOG_MASK_ON)) {
-		if (!___ratelimit(&user->rs, current->comm))
-			return ret;
-	}
+	//if (!(devkmsg_log & DEVKMSG_LOG_MASK_ON)) {
+		//if (!___ratelimit(&user->rs, current->comm))
+			//return ret;
+	//}
 
-	buf = kmalloc(len+1, GFP_KERNEL);
-	if (buf == NULL)
-		return -ENOMEM;
+	//buf = kmalloc(len+1, GFP_KERNEL);
+	//if (buf == NULL)
+		//return -ENOMEM;
 
-	buf[len] = '\0';
-	if (!copy_from_iter_full(buf, len, from)) {
-		kfree(buf);
-		return -EFAULT;
-	}
+	//buf[len] = '\0';
+	//if (!copy_from_iter_full(buf, len, from)) {
+		//kfree(buf);
+		//return -EFAULT;
+	//}
 
 	/*
 	 * Extract and skip the syslog prefix <[0-9]*>. Coming from userspace
@@ -818,25 +819,25 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 	 * enforce LOG_USER, to be able to reliably distinguish
 	 * kernel-generated messages from userspace-injected ones.
 	 */
-	line = buf;
-	if (line[0] == '<') {
-		char *endp = NULL;
-		unsigned int u;
+	//line = buf;
+	//if (line[0] == '<') {
+		//char *endp = NULL;
+		//unsigned int u;
 
-		u = simple_strtoul(line + 1, &endp, 10);
-		if (endp && endp[0] == '>') {
-			level = LOG_LEVEL(u);
-			if (LOG_FACILITY(u) != 0)
-				facility = LOG_FACILITY(u);
-			endp++;
-			len -= endp - line;
-			line = endp;
-		}
-	}
+		//u = simple_strtoul(line + 1, &endp, 10);
+		//if (endp && endp[0] == '>') {
+			//level = LOG_LEVEL(u);
+			//if (LOG_FACILITY(u) != 0)
+				//facility = LOG_FACILITY(u);
+			//endp++;
+			//len -= endp - line;
+			//line = endp;
+		//}
+	//}
 
-	printk_emit(facility, level, NULL, 0, "%s", line);
-	kfree(buf);
-	return ret;
+	//printk_emit(facility, level, NULL, 0, "%s", line);
+	//kfree(buf);
+	//return ret;
 }
 
 static ssize_t devkmsg_read(struct file *file, char __user *buf,
