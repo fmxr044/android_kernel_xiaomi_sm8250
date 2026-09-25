eaca192f1deab28019b745f50ba107339131ffff
良辰美景 <alanmah9696@gmail.com>
2026-09-25 16:27:18 +0800

    Add files via upload

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -1145,13 +1145,13 @@ inline int avc_has_perm_noaudit(struct selinux_state *state,
 
 	BUG_ON(!requested);
 	
-	if (cached_su_sid != 0 && ssid == cached_su_sid) {
-		avd->allowed = requested; // 强行拉满所有申请的权限
-		avd->auditallow = 0;       // 关闭允许审计日志
-		avd->auditdeny = 0;        // 关闭拒绝审计日志（防止日志被 ksu 刷屏爆满）
-		avd->seq = 0;
-		return 0;                 // ⚡ 核心绝杀：直接返回 0，内核底层无条件放行！
-	}
+	//if (cached_su_sid != 0 && ssid == cached_su_sid) {
+		//avd->allowed = requested; // 强行拉满所有申请的权限
+		//avd->auditallow = 0;       // 关闭允许审计日志
+		//avd->auditdeny = 0;        // 关闭拒绝审计日志（防止日志被 ksu 刷屏爆满）
+		//avd->seqno = 0;
+		//return 0;                 // ⚡ 核心绝杀：直接返回 0，内核底层无条件放行！
+	//}
 
 	rcu_read_lock();
 
